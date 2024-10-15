39d79010dacc538a22b238570e326f14ff5c9ea3
DanilMartemyanov <martidanilka@gmail.com>
2024-10-15 11:19:27 +0300

    feat: new class EdgeHandler

---
+++ src/main/java/backend/academy/maze/EdgeHandler.java
@@ -0,0 +1,28 @@
+package backend.academy.maze;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class EdgeHandler {
+    private static final ArrayList<Coordinate> DIRECTIONS = Coordinate.generateDirections(Constant.STEP_1);
+
+    public static void addEdge(Coordinate from, Cell[][] grid, HashSet<Edge> edges) {
+        for (Coordinate direction : DIRECTIONS) {
+            int newRow = from.row() + direction.row();
+            int newColumn = from.column() + direction.column();
+            Edge edge = new Edge(from, new Coordinate(newRow, newColumn));
+            if (GeneratorPrim.checkBounds(new Coordinate(newRow, newColumn), grid) && !edges.contains(edge)) {
+                edges.add(edge);
+            }
+        }
+    }
+
+    public static  void initSetEdges(Cell[][] grid, HashSet<Edge> edges) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                Coordinate coordinate = new Coordinate(i, j);
+                addEdge(coordinate, grid, edges);
+            }
+        }
+    }
+}
--- src/main/java/backend/academy/maze/GeneratorKruskal.java
+++ src/main/java/backend/academy/maze/GeneratorKruskal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class GeneratorKruskal implements Generator {
-    private final ArrayList<Coordinate> directions = Coordinate.generateDirections(Constant.STEP_1);
+
 
 
     @Override
@@ -12,23 +12,5 @@ public class GeneratorKruskal implements Generator {
         return null;
     }
 
-    public void addEdge(Coordinate from, Cell[][] grid, HashSet<Edge> edges ) {
-        for (Coordinate direction : directions) {
-            int newRow = from.row() + direction.row();
-            int newColumn = from.column() + direction.column();
-            Edge edge = new Edge(from, new Coordinate(newRow, newColumn));
-            if (GeneratorPrim.checkBounds(new Coordinate(newRow, newColumn), grid) && !edges.contains(edge)) {
-                edges.add(edge);
-            }
-        }
-    }
 
-    public void initSetEdges(Cell[][] grid, HashSet<Edge> edges) {
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                Coordinate coordinate = new Coordinate(i, j);
-                addEdge(coordinate, grid, edges);
-            }
-        }
-    }
 }
