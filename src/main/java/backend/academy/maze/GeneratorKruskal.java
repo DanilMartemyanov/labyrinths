91bc3e944b410ec4a49b28b53d2c874b2e090c96
DanilMartemyanov <martidanilka@gmail.com>
2024-10-19 12:40:02 +0300

    clean file GeneratorKruskal

--- src/main/java/backend/academy/maze/GeneratorKruskal.java
+++ src/main/java/backend/academy/maze/GeneratorKruskal.java
@@ -4,13 +4,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class GeneratorKruskal implements Generator {
-
-
-
     @Override
     public Maze generateMaze(int height, int width) {
         return null;
     }
-
-
 }
