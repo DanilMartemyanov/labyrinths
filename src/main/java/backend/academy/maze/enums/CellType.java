ec89368eabba49a45958729dae7523af12d1ae79
DanilMartemyanov <martidanilka@gmail.com>
2024-10-27 11:30:28 +0300

    fix: class Constant, fix: class CellType: added value

--- src/main/java/backend/academy/maze/enums/CellType.java
+++
@@ -1,6 +0,0 @@
-package backend.academy.maze.enums;
-
-public enum CellType {
-    WALL, PASSAGE, GLASS, BOMB, A, B, GIFT, ENTRANCE;
-
-}
--- src/main/java/backend/academy/maze/models/UnionFind.java
+++
@@ -1,9 +0,0 @@
-package backend.academy.maze.models;
-
-public interface UnionFind {
-    // объединение множеств
-    void union(int cellX, int cellY);
-
-    // проверка на принадлежность к одному множеству
-    boolean find(int cellX, int cellY);
-}
